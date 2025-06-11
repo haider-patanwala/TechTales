0f38c03f86e25724a2a8b60db37733f630a1b515
Haider Patanwala <haiderpatanwala@gmail.com>
2025-06-11 12:49:55 +0530

    Add Contentstack environment variables to .env.example and update env.js for validation

--- src/env.js
+++ src/env.js
@@ -4,11 +4,21 @@ import { z } from "zod";
 export const env = createEnv({
 	server: {
 		NODE_ENV: z.enum(["development", "test", "production"]),
+		CONTENTSTACK_HOST: z.string(),
+		CONTENTSTACK_BRANCH: z.string(),
+		CONTENTSTACK_ENVIRONMENT: z.string(),
+		CONTENTSTACK_API_KEY: z.string(),
+		CONTENTSTACK_DELIVERY_TOKEN: z.string(),
 	},
 	client: {},
 
 	runtimeEnv: {
 		NODE_ENV: process.env.NODE_ENV,
+		CONTENTSTACK_HOST: process.env.CONTENTSTACK_HOST,
+		CONTENTSTACK_BRANCH: process.env.CONTENTSTACK_BRANCH,
+		CONTENTSTACK_ENVIRONMENT: process.env.CONTENTSTACK_ENVIRONMENT,
+		CONTENTSTACK_API_KEY: process.env.CONTENTSTACK_API_KEY,
+		CONTENTSTACK_DELIVERY_TOKEN: process.env.CONTENTSTACK_DELIVERY_TOKEN,
 	},
 	skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 	emptyStringAsUndefined: true,
